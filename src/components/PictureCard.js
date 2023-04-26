ebc73fb77f209edc4f3fcd429d466d50a967ec76
Mike Melchior <mike.melchior@outlook.com>
2023-04-26 17:25:55 -0400

    add setClickCoordinates function that sets state of coordinates to user mouse click relative to img

--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function PictureCard( { puzzle } ) {
+  const [coordinates, setCoordinates] = useState({})
   
 
+  const setClickCoordinates = (e) => {
+    let rect = e.target.getBoundingClientRect()
+    let x = e.clientX - rect.left
+    let y = e.clientY - rect.top
+    
+    setCoordinates({x: x, y: y})
+  }
 
   return (
     <div className="picture-container">
-      <img id='puzzle' src={puzzle.img} alt={puzzle.name} />
+      <img 
+        id='puzzle' 
+        src={puzzle.img} 
+        alt={puzzle.name}
+        onClick={(e) => setClickCoordinates(e)}
+      />
     </div>
   )
 }
