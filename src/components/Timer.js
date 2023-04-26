9864f2f97bc980bff661086b55f31edacd1158c0
Mike Melchior <mike.melchior@outlook.com>
2023-04-26 14:49:55 -0400

    Create a timer component to keep track of time spent solving the puzzle

---
+++ src/components/Timer.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+
+
+export default function Timer() {
+  const [time, setTime] = useState(0)
+
+  let seconds = time % 60
+  let minutes = Math.floor(time/60) % 60
+  let hours = Math.floor(time/3600)
+
+  const addZero = (num) => {
+    let addZeroWithArray = ['0'];
+    addZeroWithArray.push(num.toString())
+    let result = addZeroWithArray.join('')
+    return result
+  }
+  
+  if (seconds.toString().length === 1) {
+    seconds = addZero(seconds)
+  }
+  if (minutes.toString().length === 1) {
+    minutes = addZero(minutes)
+  } 
+  if (hours.toString().length === 1) {
+    hours = addZero(hours)
+  }
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setTime((prev) => {
+        return prev + 1;
+      })
+    }, 1000)
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [time])
+  
+
+  return (
+    <div className='timer'>
+      <p>Time Spent Solving : {`${hours} : ${minutes} : ${seconds}`}</p>
+    </div>
+  )
+}
