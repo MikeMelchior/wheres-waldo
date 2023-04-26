b7ee4feda00d849583864f53ce3cab14de5532c6
Mike Melchior <mike.melchior@outlook.com>
2023-04-26 14:50:22 -0400

    puzzle data initiated with one puzzle, will be used to store future puzzles

---
+++ src/puzzles/puzzlesData.js
@@ -0,0 +1,17 @@
+import beach from './waldo-beach.jpg'
+
+
+
+const pictures = {
+  beach: {
+    name: 'beach-puzzle',
+    img: beach,
+    possibleCharacters: [
+      'Waldo'
+    ]
+  }
+
+
+}
+
+export { pictures }
