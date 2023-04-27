84d47e98bda4f060dc3b1fad7122c31fcc16b58a
Mike Melchior <mike.melchior@outlook.com>
2023-04-27 14:05:03 -0400

    accept coordinates from parent to display hit box use coordinates to inline style position of box

--- src/components/CharacterSelect.js
+++ src/components/CharacterSelect.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import uniqid from 'uniqid'
 
-export default function CharacterSelectWindow( { characters } ) {
+export default function CharacterSelectWindow( { characters, clickCoordinates } ) {
+
+  
   return (
-    <div className='character-select-window'>
-      {characters.map(character => {
-        return (
-          <div onClick={() => console.log(character)} className='character-selection'>
-            <img id='character-selection-img' src={character.img} alt={character.name} />
-            <p>{character.name}</p>
-          </div>
-        )
-      })}
+    <div style={{position: 'absolute', top: clickCoordinates.y - 20, left: clickCoordinates.x - 20}}>
+      <div className="tag-box"></div>
+      <div className='character-selections'>
+        {characters.map(character => {
+          return (
+            <div key={uniqid()} onClick={() => console.log(character)} className='character-selection'>
+              <img id='character-selection-img' src={character.img} alt={character.name} />
+              <p>{character.name}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
