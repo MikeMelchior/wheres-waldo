b6aa3f23c1c46a79540a201ec07a8aa66eba431d
Mike Melchior <mike.melchior@outlook.com>
2023-04-27 15:41:55 -0400

    add header and footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react';
 import { pictures } from '../puzzles/puzzlesData';
 import '../assets/styles.css'
-import PictureCard from './PictureCard';
-import Timer from './Timer';
-
+import Puzzle from './Puzzle';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   const [puzzle, setPuzzle] = useState()
-
 
   useEffect(() => {
     const puzzleNames = Object.keys(pictures)
@@ -16,16 +15,21 @@ function App() {
       let randomNum = Math.floor(Math.random() * puzzleNames.length)
       return puzzleNames[randomNum]
     } 
-
+    
     setPuzzle(pictures[randomPick()])
   }, [])
 
   return (
-    <>
-      <Timer /> 
-      {puzzle && <PictureCard puzzle={puzzle} />}
-    </>
-    
+    <div className='app'>
+
+       
+      {puzzle && 
+      <div className="main">
+        <Header puzzle={puzzle}/>
+        <Puzzle puzzle={puzzle}/>
+      </div>}
+      <Footer />
+    </div>
   );
 }
 
