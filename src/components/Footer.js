349a4164ab08c378a0cf801762561075698ddfb5
Mike Melchior <mike.melchior@outlook.com>
2023-04-27 15:42:04 -0400

    create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <a href="https://github.com/rubberteeth">rubberteeth</a>
+    </div>
+  )
+}
