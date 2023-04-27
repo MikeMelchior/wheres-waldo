a2934a4954c9112aa1a8cc8bd8c39a996de72315
Mike Melchior <mike.melchior@outlook.com>
2023-04-26 23:11:28 -0400

    add function to check click coordinaters for any remaining character, added notes

--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -1,24 +1,52 @@
 import React, { useState } from 'react'
 
 export default function PictureCard( { puzzle } ) {
-  const [coordinates, setCoordinates] = useState({})
-  
+  const [remainingCharacters, setRemainingCharacters] = useState(puzzle.possibleCharacters)
 
-  const setClickCoordinates = (e) => {
+  const handleClick = (e) => {
+    // use getBoundingClientRect() method to determine distance away from top & left
+    // of screen and minus them from event.client x/y coordinates to determine click
+    // location within the image
     let rect = e.target.getBoundingClientRect()
     let x = e.clientX - rect.left
     let y = e.clientY - rect.top
+
+    console.log({x: x, y: y})
     
-    setCoordinates({x: x, y: y})
+    checkCoordinatesForRemainingCharacter({x: x, y: y}, remainingCharacters)
+  }
+
+  const checkCoordinatesForRemainingCharacter = (coordinates, characterArray) => {
+    // use character object 'hit box' coordinates to determine if click event
+    // coordinates fall within character 'hit box' in the image, if they do, 
+    // assign the 'found' character to the foundCharacter variable
+    let foundCharacter = characterArray.find(char => {
+      if (coordinates.x > char.location.xMin 
+      && coordinates.x < char.location.xMax
+      && coordinates.y > char.location.yMin
+      && coordinates.y < char.location.yMax) {
+        return char
+      }
+      return false
+    })
+
+    // if character has been found, update remainingCharacters state
+    if (foundCharacter) {
+      console.log(foundCharacter);
+      setRemainingCharacters((previousRemainingCharacters) => {
+        return previousRemainingCharacters.filter(character => character !== foundCharacter)
+      })
+    } 
   }
 
   return (
     <div className="picture-container">
+      <p>Remaining Characters: {remainingCharacters.length}</p>
       <img 
         id='puzzle' 
         src={puzzle.img} 
         alt={puzzle.name}
-        onClick={(e) => setClickCoordinates(e)}
+        onClick={handleClick}
       />
     </div>
   )
