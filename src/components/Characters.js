c809feae5dcba0cfe046efffe657bae042258e18
Mike Melchior <mike.melchior@outlook.com>
2023-05-01 11:44:33 -0400

    add character prop

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import uniqid from 'uniqid'
 
-export default function Characters( { puzzle } ) {
+export default function Characters( { puzzle, characters } ) {
+
   return (
     <div>
-      {puzzle.possibleCharacters.map(character => {
+      {characters.map(character => {
         return (
-          <div>
+          <div key={uniqid()} style={{opacity: character.found ? 0.2 : 1}}>
             <img id='character-avatar-header' src={character.img} alt={character.name} />
             <p>{character.name}</p>
           </div>
