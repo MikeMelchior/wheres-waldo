9b3ec131ad3ec56406e41a82df8f370d7abc6d37
Mike Melchior <mike.melchior@outlook.com>
2023-05-02 10:33:46 -0400

    add google auth for signing in

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,4 @@
-import React, { useEffect }  from 'react'
+import React, { useEffect, useState }  from 'react'
 import {
   GoogleAuthProvider,
   signInWithPopup,
@@ -8,55 +8,65 @@ import {
 } from 'firebase/auth';
 
 export default function Signup( { player, setPlayer } ) {
-  
-  
-    // set player using google auth (firebase)
-    useEffect(() => {
-      setPlayer(getAuth().currentUser)
-    }, [setPlayer])
-  
-      // sign in using google auth
-    async function signIn() {
-    let provider = new GoogleAuthProvider();
-    await signInWithPopup(getAuth(), provider)
-    document.querySelector('.sign-in').classList.add('hidden')
-    document.querySelector('.sign-out').classList.remove('hidden')
-    }
+  const [loading, setLoading] = useState(true)
 
-    function signOutUser() {
-      signOut(getAuth())
-    }
-  
-    useEffect(() => {
-        // observe state change  of user
-      function authStateObserver(user) {
-        if (user) {
-          setPlayer(user)
-        } else {
-          setPlayer(null)
-        }
+  // set player using google auth (firebase)
+  useEffect(() => {
+    setPlayer(getAuth().currentUser)
+  }, [setPlayer])
+
+  useEffect(() => {
+      // observe state change of user
+    function authStateObserver(user) {
+      setTimeout(() => {
+        setLoading(false)
+      }, 500);
+      if (user) {
+        setPlayer(user)
+      } else {
+        setPlayer(null)
       }
 
-      function initFirebaseAuth() {
-        onAuthStateChanged(getAuth(), authStateObserver)
-      }
-      initFirebaseAuth()
-    },[player, setPlayer])
+    }
+
+    function initFirebaseAuth() {
+      onAuthStateChanged(getAuth(), authStateObserver)
+    }
+    initFirebaseAuth()
+  },[player, setPlayer])
+
+    // sign in using google auth
+  async function signIn() {
+  let provider = new GoogleAuthProvider();
+  await signInWithPopup(getAuth(), provider)
+  }
+
+  function signOutUser() {
+    signOut(getAuth())
+  }
+
+
 
   return (
     <>
       {player
       ?
-      <button className="sign-out" onClick={signOutUser} >Sign Out</button>
+        <button className="sign-out" onClick={signOutUser} >Sign Out</button>
       : 
-      <div className='sign-in-page'>
-        <div className="card">
-          <h2>Sign in with your Google Account</h2>
-          <button onClick={signIn}>Sign In</button>
-        </div>
-      </div>
+        <>
+          {loading
+          ?
+            <div className='loading'>Loading . . .</div>
+          :
+            <div className='sign-in-page'>
+              <div className="card">
+                <h2>Sign in with your Google Account</h2>
+                <button onClick={signIn}>Sign In</button>
+              </div>
+            </div>
+          }
+        </>
       }
     </>
-    
   )
 }
