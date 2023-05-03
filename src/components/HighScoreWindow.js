3bacec0b6dcb58d818ecfecc8ba1dbc8023302f6
Mike Melchior <mike.melchior@outlook.com>
2023-05-03 14:01:51 -0400

    create high score window component that uses the database ref to pull 'high scores' from db and display them onscreen after submitting new score

---
+++ src/components/HighScoreWindow.js
@@ -0,0 +1,104 @@
+import { 
+  query,
+  collection,
+  getFirestore,
+  orderBy,
+  limit,
+  onSnapshot,
+  doc,
+  updateDoc
+  } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+
+
+
+export default function HighScoreWindow( { player, puzzle, dbRefID } ) {
+  const [name, setName] = useState()
+  const [submitted, setSubmitted] = useState(false)
+  let docRef = doc(getFirestore(), `${puzzle.name}`, dbRefID)
+
+  useEffect(() => {
+    setName(player.displayName)
+  },[player.displayName])
+
+
+
+  function getScores() {
+    // Create the query to load the last 12 messages and listen for new ones.
+    const recentMessagesQuery = query(collection(getFirestore(), `${puzzle.name}`), orderBy('score', 'asc'), limit(8));
+    
+    // Start listening to the query.
+    onSnapshot(recentMessagesQuery, function(snapshot) {
+      snapshot.docChanges().forEach(function(change) {
+        let gameData = change.doc.data();
+        if (gameData.nickname && gameData.score) createScoreDiv(gameData.nickname, gameData.score)
+      });
+    });
+  }
+
+
+  function createScoreDiv(name, score) {
+    let element = document.createElement('div');
+      element.classList.add('single-high-score')
+    let nameElement = document.createElement('p');
+      nameElement.textContent = name
+    let scoreElement = document.createElement('p');
+      scoreElement.textContent = score
+
+    element.append(nameElement, scoreElement)
+    document.querySelector('.scores-display').append(element)
+  }
+
+
+
+  const handleSubmit = () => {
+    if (name === '') return alert('Please enter a name')
+
+    try {
+      updateDoc(docRef, {
+        nickname: name
+      })
+    } catch (e) {
+      console.log('Failed to update document: ', e)
+    }
+
+    setTimeout(() => {
+      getScores()
+      setName('')
+      setSubmitted(true)
+    }, 200)
+    
+  }
+
+  return (
+    <div className='high-score-window'>
+      <h1>{`${puzzle.name.toUpperCase()}`}</h1>
+      {!submitted
+      ?<>
+        <label htmlFor='name'> Enter your name:
+          <input 
+            type="text" 
+            id='name' 
+            name='name' 
+            value={`${name}`}
+            onChange={e => setName(e.target.value)}
+          />
+        </label>
+        <button 
+          onClick={handleSubmit} 
+          disabled={submitted}
+        >Submit</button>
+      </>
+      
+      : 
+        <div className="scores-display">
+          <p>High Scores</p>
+        </div>
+      }
+      
+      
+      
+    </div>
+    
+  )
+}
